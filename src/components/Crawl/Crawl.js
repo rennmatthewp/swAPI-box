9eac6228e7e275e5bf5eca6a4f6003d6f848d7d0
Matthew Renn <rennmatthewp@gmail.com>
2017-12-10 11:31:42 -0700

    renders crawlText in proper format

--- src/components/Crawl/Crawl.js
+++ src/components/Crawl/Crawl.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Crawl = ({crawl, title, year, }) => {
-  console.log(crawl);
-  
+const Crawl = ({ crawl, title, year }) => {
+  let paragraphs;
+
+  if (crawl) {
+    paragraphs = crawl.map((paragraph, index) => {
+      return <p key={`p-${index}`}>{paragraph}</p>;
+    });
+  }
+
   return (
     <div>
-      <p>{crawl}</p>
+      {paragraphs}
       <h3>{title}</h3>
       <h4>{year}</h4>
     </div>
-  )
-}
+  );
+};
+
+Crawl.propTypes = {
+  crawlText: PropTypes.object,
+  crawl: PropTypes.array,
+  title: PropTypes.string,
+  year: PropTypes.string
+};
 
 export default Crawl;
