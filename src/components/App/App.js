9529382a853c8d7c2120c1982dedd64a34fa9907
Matthew Renn <rennmatthewp@gmail.com>
2017-12-10 09:43:01 -0700

    changes 'crawl' to 'film' for ease of naming

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Header from '../../containers/Header/Header';
 import CardContainer from '../../containers/CardContainer/CardContainer';
 
+import { getFilm } from "../../helper";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      crawl: [],
+      film: [],
       people: [],
       planets: [],
       vehicles: [],
@@ -14,32 +16,33 @@ class App extends Component {
     };
   }
   
-  componentDidMount() {
-    this.getCrawl()
+  async componentDidMount() {
+    const film = await getFilm();
+    this.setState({ film })
   }
 
-  getCrawl() {
-    fetch('https://swapi.co/api/films/')
-    .then((result) => result.json())
-    .then(this.formatCrawl)    
-    .then((crawl) => this.setState({crawl}))
-  }
+  // getCrawl() {
+  //   fetch('https://swapi.co/api/films/')
+  //   .then((result) => result.json())
+  //   .then(this.formatCrawl)    
+  //   .then((crawl) => this.setState({crawl}))
+  // }
 
-  formatCrawl(response) {
-    return response.results.map( film => {
-      return {  title: film.title,
-                filmNum: film.episode_id,
-                body: film.opening_crawl,
-                date: film.created
-            }
-    })
-  }
+  // formatCrawl(response) {
+  //   return response.results.map( film => {
+  //     return {  title: film.title,
+  //               filmNum: film.episode_id,
+  //               body: film.opening_crawl,
+  //               date: film.created
+  //           }
+  //   })
+  // }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <CardContainer crawlText={this.state.crawl} />
+        <CardContainer crawlText={this.state.film} />
       </div>
     );
   }
