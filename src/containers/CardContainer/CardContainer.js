c686e89d8320c4a07f5e12cd73fa3eca4ca7bdf1
Matthew Renn <rennmatthewp@gmail.com>
2017-12-10 09:43:50 -0700

    removes imported Crawl package

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Crawl from "react-star-wars-crawl";
 
 const CardContainer = (crawlText) => {
-
-  const { title, body, date } = crawlText;
   return (
     <div>
-      <Crawl
-          subTitle={title}
-          text={body}
-          date={date}
-      />;
+      
     </div>
   );
 };
