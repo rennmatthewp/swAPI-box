85db8b0ed09de58bc71a8b1d629ecc070f95c359
Matthew Renn <rennmatthewp@gmail.com>
2017-12-11 12:55:53 -0700

    fetches and formats people array from api

--- src/helper.js
+++ src/helper.js
@@ -1,18 +1,22 @@
-const fetchAndParse = async url => {
-  const initialFetch = await fetch(url);
-  return initialFetch.json();
+export const fetchAndParse = async url => {
+  try {
+    const initialFetch = await fetch(url);
+    return initialFetch.json();
+  } catch (error) {
+    return 'error:', error;
+  }
 };
-
-const filmNum = Math.ceil(Math.random() * 7);
 
 export const getFilm = async () => {
   try {
     const film = await fetchAndParse(`https://swapi.co/api/films/${filmNum}`);
     return formatFilmObject(film);
   } catch (error) {
-    return ('error:', error);
+    return 'error:', error;
   }
 };
+
+const filmNum = Math.ceil(Math.random() * 7);
 
 const formatFilmObject = ({ title, opening_crawl, release_date }) => {
   return {
@@ -22,12 +26,37 @@ const formatFilmObject = ({ title, opening_crawl, release_date }) => {
   };
 };
 
-const releaseYear = fullDate => {
-  return fullDate.split('-')[0];
-};
+const releaseYear = fullDate => fullDate.split('-')[0];
 
 const formatCrawlText = rawCrawlText => {
   const regEx = new RegExp(/\s{3,}/, 'g');
   let crawl = rawCrawlText.replace(regEx, '###');
   return (crawl = crawl.split('###'));
+};
+
+export const getPeople = async () => {
+  const peopleObject = await fetchAndParse('https://swapi.co/api/people');
+  return formatPeople(peopleObject.results);
+};
+
+const formatPeople = peopleArray => {
+  const unresolvedPromises = peopleArray.map(async person => {
+    const { name, homeworld, species } = person;
+    let homeworldData = await fetchAndParse(homeworld);
+    let speciesData = await fetchAndParse(species);
+
+    return {
+      name,
+      stats: {
+        homeworld: homeworldData.name,
+        species: speciesData.name,
+        population: homeworldData.population,
+        language: speciesData.language
+      },
+      favorite: false,
+      cardCategory: 'people'
+    };
+  });
+
+  return Promise.all(unresolvedPromises);
 };
